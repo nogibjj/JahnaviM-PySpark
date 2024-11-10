8ec23b3537fd42da632b5eae899d660cc04c8388
Jahnavi Maddhuri <jahnavi.maddhuri@gmail.com>
2024-11-10 12:53:04 -0500

    update extract for large dataset

--- mylib/extract.py
+++ mylib/extract.py
@@ -1,8 +1,8 @@
 '''Extracts a dataset'''
 import requests
 
-URL = 'https://raw.githubusercontent.com/fivethirtyeight/data/refs/heads/master/bad-drivers/bad-drivers.csv'
-FILE_PATH = 'data/bad-drivers.csv'
+URL = 'https://raw.githubusercontent.com/fivethirtyeight/election-results/refs/heads/main/races.csv'
+FILE_PATH = 'data/election-results.csv'
 
 def extract(url = URL, path = FILE_PATH):
     '''Extracts a dataset based on URL and saves it to the path'''
@@ -10,3 +10,6 @@ def extract(url = URL, path = FILE_PATH):
         with open(path, 'wb') as f:
             f.write(r.content)
     return path
+
+if __name__ == '__main__':
+    extract()
